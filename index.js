91e50d6466fe8fa8f1e9f87f204f6f3305d1c30d
aquilaTouati <akilatouati97@gmail.com>
2021-12-24 22:35:26 +0100

    new

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import postRoutes from './routers.js/router2.js';
 import userRoutes from './routers.js/router3.js';
 
 const app = express();
-
+//body parser helps to properly send the requests
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
@@ -18,8 +18,9 @@ app.use('/user', userRoutes);
 const CONNECTION_URL = 'mongodb+srv://akilol:strawberry1997@cluster0.csnp4.mongodb.net/test';
 const PORT = process.env.PORT|| 5000;
 
+//giving 2 parameters, connection URL and parser Object
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+ /*promise*/ .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 // mongoose.set('useFindAndModify', false);
--- middlewares.js/auth.js
+++ middlewares.js/auth.js
@@ -1,26 +1,26 @@
-// import jwt from "jsonwebtoken";
+// // import jwt from "jsonwebtoken";
 
-// const secret = 'test';
-// const auth = async (req, res, next) => {
-//   try {
-// const token = req.headers.authorization/*.split(" ")[1]*/;
-// console.log("token:", token);
-// const isCustomAuth = token.length < 500;
+// // const secret = 'test';
+// // const auth = async (req, res, next) => {
+// //   try {
+// // const token = req.headers.Authorization.split(" ")[1];
+// // console.log("token:", token);
+// // const isCustomAuth = token.length < 500;
 
-// let decodedData;
-// if(token && isCustomAuth ){
-//   decodedData= JSON.parse(jwt.verify(token, secret));
-//   if (decodedData){
-//     req.userId=decodedData.id;
-//   }
-// } else {
-//   decodedData= JSON.parse(jwt.decode(token));
-//   req.userId= decodedData.sub;
-// }
-//     next();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+// // let decodedData;
+// // if(token && isCustomAuth ){
+// //   decodedData= JSON.parse(jwt.verify(token, secret));
+// //   if (decodedData){
+// //     req.userId=decodedData.id;
+// //   }
+// // } else {
+// //   decodedData= JSON.parse(jwt.decode(token));
+// //   req.userId= decodedData.sub;
+// // }
+// //     next();
+// //   } catch (error) {
+// //     console.log(error);
+// //   }
+// // };
 
-// export default auth;
+// // export default auth;
